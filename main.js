b7218d57349acf3b137026757056af2c543d2e0a
Stephane Baroux <stephane.baroux@gmail.com>
2021-04-22 22:13:28 +0200

    working mobile version with functioning js script and accurate css. media querie for desktop version still to do

---
+++ main.js
@@ -0,0 +1,26 @@
+const toggleButton = document.getElementById("checkbox");
+const priceSlider = document.getElementById("priceSlider");
+const pageviews = document.getElementById("pageviews");
+const price = document.getElementById("price");
+const billingCycle = document.getElementById("billingCycle");
+const pageviewsArray = ["10k PAGEVIEWS", "50k PAGEVIEWS", "100k PAGEVIEWS", "500k PAGEVIEWS", "1M PAGEVIEWS"];
+const priceArrayMonthly = ["8", "12", "16", "24", "36"];
+
+
+function setPrice(){
+    let selectorLocation = parseInt(priceSlider.value) - 1;
+    pageviews.innerHTML = pageviewsArray[selectorLocation];
+
+    if(toggleButton.checked === true ){
+        let MonthlyPrice = parseInt(priceArrayMonthly[selectorLocation]) / 1.25;
+        let annualPrice = MonthlyPrice * 12;
+        price.innerHTML = "$" + annualPrice.toFixed(2);
+        billingCycle.innerHTML = "/year";
+    } else {
+        price.innerHTML = "$" + parseInt(priceArrayMonthly[selectorLocation]).toFixed(2);
+        billingCycle.innerHTML = "/month";
+    }
+}
+
+priceSlider.addEventListener("input", setPrice);
+toggleButton.addEventListener("input", setPrice);
